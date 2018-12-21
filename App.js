c0e38e5668c8bcc94d0f33b92a65a7735a479476
Alexey Belov <alexey.belov@nordigy.ru>
2018-12-21 22:00:17 +0300

    add navigation and components

--- App.js
+++ App.js
@@ -1,11 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Platform, StatusBar, StyleSheet, Text } from 'react-native';
+import DeckList from './components/DeckList';
+import AddDeck from './components/AddDeck';
+import Deck from './components/Deck';
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createStackNavigator,
+} from 'react-navigation';
+import { Constants } from 'expo';
+import { green, white } from './utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
+const Tabs = createBottomTabNavigator(
+  {
+    DeckList: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: 'Decks',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        ),
+      },
+    },
+    AddDeck: {
+      screen: AddDeck,
+      navigationOptions: {
+        tabBarLabel: 'Add Deck',
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    navigationOptions: {
+      header: null,
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? green : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ? white : green,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    },
+  },
+);
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  Deck: {
+    screen: Deck,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: green,
+      },
+    },
+  },
+});
+const AppContainer = createAppContainer(MainNavigator);
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <UdaciStatusBar backgroundColor={green} barStyle="light-content" />
+        <AppContainer />
       </View>
     );
   }
