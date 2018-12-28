9594996eebff8d1fe0407c99a3501f8d4edb8cc7
Alexey Belov <alexey.belov@nordigy.ru>
2018-12-28 19:53:29 +0300

    add quiz

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
 
 class Quiz extends Component {
+  state = {
+    showQuestion: true,
+    cardNumber: 0,
+    rightAnswers: 0,
+  };
+  static navigationOptions = ({ navigation }) => {
+    const { title } = navigation.state.params;
+    return { title };
+  };
   render() {
+    const { deck } = this.props;
+    const countOfCard = deck.questions.length;
+    const { showQuestion, cardNumber, rightAnswers } = this.state;
+    if (countOfCard === 0) {
+      return (
+        <View>
+          <Text>Sorry, you cannot take a quiz because there are no cards in the deck</Text>
+        </View>
+      );
+    }
+    if (cardNumber === countOfCard) {
+      return (
+        <View>
+          <Text>
+            Congratulation! You finished Quiz and your results - {rightAnswers} of {countOfCard}
+          </Text>
+        </View>
+      );
+    }
     return (
       <View>
-        <Text>number/count</Text>
-        <TouchableOpacity>
-          <Text>What a fuck?</Text>
+        <Text>
+          {cardNumber + 1}/{countOfCard}
+        </Text>
+        <View>
+          <Text>
+            {showQuestion ? deck.questions[cardNumber].question : deck.questions[cardNumber].answer}
+          </Text>
+        </View>
+        <TouchableOpacity onPress={() => this.setState({ showQuestion: !showQuestion })}>
+          <Text>{showQuestion ? 'Answer' : 'Question'}</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Answer</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>
+            this.setState({ cardNumber: cardNumber + 1, rightAnswers: rightAnswers + 1 })
+          }
+        >
           <Text>Correct</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => this.setState({ cardNumber: cardNumber + 1 })}>
           <Text>UnCorrect</Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
+function mapStateToProps(state, { navigation }) {
+  const { title } = navigation.state.params;
+  const deck = state[title];
+  return {
+    title,
+    deck,
+  };
+}
 
-export default Quiz;
+export default connect(mapStateToProps)(Quiz);
